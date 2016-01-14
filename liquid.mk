f7ec19f1b47ebf1a8a5058b09baac07cf9bdb4b0
dhruvmu <dhruvmu@hotmail.com>
2016-01-14 15:39:56 +0100

    liquify condor

---
+++ liquid.mk
@@ -0,0 +1,12 @@
+$(call inherit-product, device/motorola/condor/full_condor.mk)
+
+# Inherit some common liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+
+# Bootanimation
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := liquid_condor
+PRODUCT_RELEASE_NAME := MOTO E
