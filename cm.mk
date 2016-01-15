5c822a3ee9d2d5041ff2dbb932dccdf87f22ad76
starkdroid <gamerpince.exp@gmail.com>
2016-01-15 09:15:19 +0100

    shit cleanup

--- cm.mk
+++ cm.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
-# Inherit some common liquid stuff.
-$(call inherit-product, vendor/liquid/config/common_phone.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Bootanimation
 TARGET_SCREEN_HEIGHT := 960
 TARGET_SCREEN_WIDTH := 540
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := liquid_condor
+PRODUCT_NAME := cm_condor
 PRODUCT_RELEASE_NAME := MOTO E
