54a6958ecee12d447401b1893f06b3a520d18da4
starkdroid <gamerprince.exp@gmail.com>
2016-10-06 20:31:49 +0530

    prepare for N

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,5 +41,5 @@ TARGET_OTA_ASSERT_DEVICE := xt1021,xt1022,xt1023,condor_umts,condor_umtsds,condo
 TARGET_INIT_VENDOR_LIB := libinit_condor
 TARGET_RECOVERY_DEVICE_MODULES := libinit_condor
 
-BOARD_SEPOLICY_DIRS += \
+# BOARD_SEPOLICY_DIRS += \
     device/motorola/condor/sepolicy
